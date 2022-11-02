8c22c84fcfd2fba079bafedc76dfe788167d6332
1031531798 <1031531798@qq.com>
2022-11-02 17:05:22 +0800

    ✨ feat(RTC): 多人聊天开发
    优化：断开连接后清除房间用户

--- utils/socket.js
+++ utils/socket.js
@@ -4,19 +4,24 @@ const { Server } = require("socket.io");
 const { RtcRoom } = require('../RTC/room/room');
 const { stringify } = require('./utile');
 const roomList = []
+const userRoom = {}
 function initSocket (app) {
   const server = http.createServer(app);
   const io = new Server(server, { cors: {
     credentials: true,
     // 跨域过滤源
-    origin: ["http://localhost:8796", 'http://localhost:3100', 'http://localhost:8080'],
+    origin: ["http://localhost:8796", 'http://localhost:3100', 'http://localhost:8080', 'http://192.168.19.129:8080'],
   } });
   io.on('connection', (socket) => {
     console.log('用户已连接socket');
     socket.on('disconnect',()=>{
       //监听用户断开事件
+      console.log("用户"+socket.id+"断开连接", );
+      // 清除 房间内的用户
+      const roomId = socket.data.roomId
+      const room = roomList.find(item => item.id === roomId)
+      room && room.exit(socket.id)
       io.in(socket.id).disconnectSockets()
-      console.log("用户"+socket.id+"断开连接", socket.rooms);
     });
      // 连接就调用登录
      //监听login事件 返回socket用户id
@@ -48,6 +53,8 @@ function initSocket (app) {
       if (flag) {
         io.in(socket.id).socketsJoin(roomId)
         io.to(roomId).emit('roomChange', stringify(room.getRoomData()))
+        // 保存进入的房间id
+        socket.data.roomId = roomId
       }
       socket.emit('join', stringify({
         success: flag,
@@ -65,14 +72,23 @@ function initSocket (app) {
       }
       // 给对应房间 推送事件
       if (flag) {
-        io.to(roomId).emit('roomChange', stringify(room.getRoomData()))
         socket.leave(roomId)
+        io.to(roomId).emit('roomChange', stringify(room.getRoomData()))
       }
       console.log('离开房间',socket.rooms)
     })
     socket.on('roomList', (data) => {
       socket.emit('roomList', stringify(roomList))
     })
+    socket.on('offer', data=>{
+      socket.to(data.roomid).emit('offer',data);
+    });
+    socket.on('answer', data=>{
+        socket.to(data.roomid).emit('answer',data);
+    });
+    socket.on('__ice_candidate', data=>{
+        socket.to(data.roomid).emit('__ice_candidate',data);
+    });
   })
   io.listen(3000);
 }
